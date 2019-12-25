b519e7b87d8b828e49edf10eacb2fcedef4a0fa6
Anurag <mikkydhiman@gmail.com>
2019-12-25 17:09:37 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 require('./services/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+mongoose.connect(keys.mongoURI);
 const app = express();
-require('./routes/authRoutes')(app);
-
-
 
 	
-const PORT = process.env.PORT || 5000;
+require('./routes/authRoutes')(app);
+const PORT = process.env.PORT || 8888;
 app.listen(PORT);
